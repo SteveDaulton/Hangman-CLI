97999d730c3090fb81ff531726a08688bb989c7e
Steve Daulton <stevedaulton@gmail.com>
2023-11-16 20:21:41 +0000

    Sphinx Docs
    Auto-documentation with Sphinx.

---
+++ docs/source/conf.py
@@ -0,0 +1,44 @@
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+-- Project information -----------------------------------------------------
+https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../hangman'))
+
+
+project = 'Hangman-CLI'
+author = 'Steve Daulton'
+release = '1.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.coverage',
+              ]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_theme_options = {'logo': 'hangman.png'}
+
+# html_theme = 'scrolls'
+# html_theme = 'sphinxdoc'
+
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. Hangman-CLI documentation master file, created by
+   sphinx-quickstart on Thu Nov 16 18:21:23 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Hangman-CLI
+======================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+Index
+-----
+
+* :ref:`genindex`
+
+Documentation
+-------------
+
+.. automodule:: hangman
+     :members:
