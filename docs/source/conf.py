69f5c03da98f7b6df30bcf534552b0ea3f14c654
Steve Daulton <stevedaulton@gmail.com>
2023-12-05 02:00:54 +0000

    Reset PyCharm config.
    Fix problems with MyPy false positives.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,7 @@ https://www.sphinx-doc.org/en/master/usage/configuration.html
 https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 """
 # pylint: disable=C
+# mypy: ignore-errors
 
 import os
 import sys
